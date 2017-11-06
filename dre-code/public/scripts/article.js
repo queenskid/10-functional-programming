d4dbb31f9d8c250122813b834556d54f8b498a6e
QueensKid <andres.ibarra0716@gmail.com>
2017-11-06 14:56:13 -0800

    adding some more stuff

--- dre-code/public/scripts/article.js
+++ dre-code/public/scripts/article.js
@@ -7,7 +7,7 @@ var app = app || {};
 // Where the IIFE is invoked, pass in the global 'app' object that is defined above.
 (function (module) {
   function Article(rawDataObj) {
-    /* REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context.
+    /* REVIEWED: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context.
     Normally, "this" inside of a constructor function refers to the newly instantiated object.
     However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object.
     One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function.
@@ -36,7 +36,7 @@ var app = app || {};
     /* OLD forEach():
     rawData.forEach(articleObject => Article.all.push(new Article(articleObject)))4
     */
-    Article.all = rows.map(articleObject => new Article(articleObject));
+    Article.all = rawData.map(articleObject => new Article(articleObject));
   };
 
   Article.fetchAll = callback => {
@@ -51,7 +51,7 @@ var app = app || {};
   Article.numWordsAll = () => {
     return Article.all.map().(current => {
       return (current.title.split(/\s+/).length) + (current.body.split(/\s+/).length);
-    })/reduce((accumulator + current))
+    }).reduce((accumulator + current))
   });
 };
 
@@ -99,13 +99,13 @@ Article.truncateTable = callback => {
     method: 'DELETE',
   })
   .then(console.log)
-  // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
+  // REVIEWED: Check out this clean syntax for just passing 'assumed' data into a named function!
   // The reason we can do this has to do with the way Promise.prototype.then() works. It's a little outside the scope of 301 material, but feel free to research!
   .then(callback);
 };
 
 Article.prototype.insertRecord = function(callback) {
-  // REVIEW: Why can't we use an arrow function here for .insertRecord()?
+  // REVIEWED: Why can't we use an arrow function here for .insertRecord()?
   $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
   .then(console.log)
   .then(callback);
