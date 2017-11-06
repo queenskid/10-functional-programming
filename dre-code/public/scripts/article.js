fbab25049e06b53805b17616b2b0d624d96511e0
QueensKid <andres.ibarra0716@gmail.com>
2017-11-06 14:32:10 -0800

    changes to code

--- dre-code/public/scripts/article.js
+++ dre-code/public/scripts/article.js
@@ -30,7 +30,7 @@ var app = app || {};
   Article.loadAll = rawData => {
     rawData.sort((a,b) => (new Date(b.publishedOn)) - (new Date(a.publishedOn)))
 
-    // TODO: Refactor this .forEach() code, by using a .map() call instead, since what we are trying to accomplish is the transformation of one collection into another. Remember that we can set variables equal to the result of functions. So if we set a variable equal to the result of a .map(), it will be our transformed array.
+    // DONE: Refactor this .forEach() code, by using a .map() call instead, since what we are trying to accomplish is the transformation of one collection into another. Remember that we can set variables equal to the result of functions. So if we set a variable equal to the result of a .map(), it will be our transformed array.
     // There is no need to push to anything.
 
     /* OLD forEach():
@@ -53,90 +53,90 @@ var app = app || {};
       return (current.title.split(/\s+/).length) + (current.body.split(/\s+/).length);
     })/reduce((accumulator + current))
   });
-  };
+};
 
-  // DONE: Chain together a .map() and a .reduce() call to produce an array of unique author names. You will probably need to use the optional accumulator argument in your reduce call.
-  Article.allAuthors = () => {
-    return Article.all.map().(current => current.author).reduce((accumulator + current) => {
-      if (accumulator === []) {
-        return accumulator.concat([current]);
-      }
-      if (!accumulator.inlcudes(current)) {
-        return accumulator.concat([current]);
-      }
-      return accumulator;
-    }, []);
-  };
+// DONE: Chain together a .map() and a .reduce() call to produce an array of unique author names. You will probably need to use the optional accumulator argument in your reduce call.
+Article.allAuthors = () => {
+  return Article.all.map().(current => current.author).reduce((accumulator + current) => {
+    if (accumulator === []) {
+      return accumulator.concat([current]);
+    }
+    if (!accumulator.inlcudes(current)) {
+      return accumulator.concat([current]);
+    }
+    return accumulator;
+  }, []);
+};
 
-  Article.numWordsByAuthor = () => {
-    return Article.allAuthors().map(author => {
-      // DONE: Transform each author string into an object with properties for the author's name, as well as the total number of words across all articles written by the specified author.
-      // HINT: This .map() should be set up to return an object literal with two properties.
-      // The first property should be pretty straightforward, but you will need to chain some combination of .filter(), .map(), and .reduce() to get the value for the second property.
-      let filteredAuthors = Article.all.map((current) => {
-        return {
-          name: current.author,
-          words: (current.title.split(/\s+/).length) + (current.body.split(/\s+/).length).
-          url: current.authorUrl
-        }
-      }).filter(current => {
-        return current.name === author;
-      })
-      let numWords = filteredAuthors.reduce((acc, cur) =>{
-        return acc + cur.words;
-      }, 0)
+Article.numWordsByAuthor = () => {
+  return Article.allAuthors().map(author => {
+    // DONE: Transform each author string into an object with properties for the author's name, as well as the total number of words across all articles written by the specified author.
+    // HINT: This .map() should be set up to return an object literal with two properties.
+    // The first property should be pretty straightforward, but you will need to chain some combination of .filter(), .map(), and .reduce() to get the value for the second property.
+    let filteredAuthors = Article.all.map((current) => {
       return {
-        author: author;
-        totalWords: numWords;
-        url:filteredAuthors[0].url
-      };
-    });
-  };
-
-  Article.truncateTable = callback => {
-    $.ajax({
-      url: '/articles',
-      method: 'DELETE',
-    })
-    .then(console.log)
-    // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
-    // The reason we can do this has to do with the way Promise.prototype.then() works. It's a little outside the scope of 301 material, but feel free to research!
-    .then(callback);
-  };
-
-  Article.prototype.insertRecord = function(callback) {
-    // REVIEW: Why can't we use an arrow function here for .insertRecord()?
-    $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
-    .then(console.log)
-    .then(callback);
-  };
-
-  Article.prototype.deleteRecord = function(callback) {
-    $.ajax({
-      url: `/articles/${this.article_id}`,
-      method: 'DELETE'
-    })
-    .then(console.log)
-    .then(callback);
-  };
-
-  Article.prototype.updateRecord = function(callback) {
-    $.ajax({
-      url: `/articles/${this.article_id}`,
-      method: 'PUT',
-      data: {
-        author: this.author,
-        authorUrl: this.authorUrl,
-        body: this.body,
-        category: this.category,
-        publishedOn: this.publishedOn,
-        title: this.title,
-        author_id: this.author_id
+        name: current.author,
+        words: (current.title.split(/\s+/).length) + (current.body.split(/\s+/).length).
+        url: current.authorUrl
       }
+    }).filter(current => {
+      return current.name === author;
     })
-    .then(console.log)
-    .then(callback);
-  };
+    let numWords = filteredAuthors.reduce((acc, cur) =>{
+      return acc + cur.words;
+    }, 0)
+    return {
+      author: author;
+      totalWords: numWords;
+      url:filteredAuthors[0].url
+    };
+  });
+};
 
-  module.Article = Article;
-}) (app);
+Article.truncateTable = callback => {
+  $.ajax({
+    url: '/articles',
+    method: 'DELETE',
+  })
+  .then(console.log)
+  // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
+  // The reason we can do this has to do with the way Promise.prototype.then() works. It's a little outside the scope of 301 material, but feel free to research!
+  .then(callback);
+};
+
+Article.prototype.insertRecord = function(callback) {
+  // REVIEW: Why can't we use an arrow function here for .insertRecord()?
+  $.post('/articles', {author: this.author, authorUrl: this.authorUrl, body: this.body, category: this.category, publishedOn: this.publishedOn, title: this.title})
+  .then(console.log)
+  .then(callback);
+};
+
+Article.prototype.deleteRecord = function(callback) {
+  $.ajax({
+    url: `/articles/${this.article_id}`,
+    method: 'DELETE'
+  })
+  .then(console.log)
+  .then(callback);
+};
+
+Article.prototype.updateRecord = function(callback) {
+  $.ajax({
+    url: `/articles/${this.article_id}`,
+    method: 'PUT',
+    data: {
+      author: this.author,
+      authorUrl: this.authorUrl,
+      body: this.body,
+      category: this.category,
+      publishedOn: this.publishedOn,
+      title: this.title,
+      author_id: this.author_id
+    }
+  })
+  .then(console.log)
+  .then(callback);
+};
+
+module.Article = Article;
+})(app);
